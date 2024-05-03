0c430a43a51464ccbf2756474d394a718db0b6ca
Acaraballo <acarabao1201@outlook.com>
2024-05-03 04:10:18 -0500

    Arquetipo

--- src/test/java/co/com/reto_siigo_automation/certificacion/runners/AutenticacionRunner.java
+++ src/test/java/co/com/reto_siigo_automation/certificacion/runners/AutenticacionRunner.java
@@ -1,14 +1,12 @@
 package co.com.reto_siigo_automation.certificacion.runners;
 
 import io.cucumber.junit.CucumberOptions;
-import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@Slf4j
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/autenticacion/autenticacion.feature",
+        features = "src/test/resources/features/auto_web/autenticacion/autenticacion.feature",
         glue = "co/com/reto_siigo_automation/certificacion/stepDefinitions",
         snippets = CucumberOptions.SnippetType.CAMELCASE)
 
